40cfb03b54be71d026f25878f09f9d692dfdaddc
Gonzalo De Los Reyes <3bgonzalodelosreyes@gmail.com>
2024-02-14 13:22:39 +0100

    Center skills positions

--- script.js
+++ script.js
@@ -26,29 +26,3 @@ function seleccionar() {
   document.getElementById("nav").classList = "";
   menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades() {
-  var skills = document.getElementById("skills");
-  var distancia_skills =
-    window.innerHeight - skills.getBoundingClientRect().top;
-  if (distancia_skills >= 300) {
-    let habilidades = document.getElementsByClassName("progreso");
-    habilidades[0].classList.add("java");
-    habilidades[1].classList.add("python");
-    habilidades[2].classList.add("c-");
-    habilidades[3].classList.add("c");
-    habilidades[4].classList.add("sql");
-    habilidades[5].classList.add("htmlcss");
-    habilidades[6].classList.add("efectividad");
-    habilidades[7].classList.add("trabajo");
-    habilidades[8].classList.add("creatividad");
-    habilidades[9].classList.add("dedicacion");
-    habilidades[10].classList.add("adaptacion");
-    habilidades[11].classList.add("resolutivo");
-  }
-}
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function () {
-  efectoHabilidades();
-};
