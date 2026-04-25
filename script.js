a93d99df2dbe7bde40faca875a63be25cc88f32a
Gonzalo De Los Reyes <3bgonzalodelosreyes@gmail.com>
2026-04-25 11:43:03 +0200

    Clean commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/** @format */
+
+// Descargar cv
+function abrirPDF() {
+  var rutaPDF = "src/CV.pdf";
+  window.open(rutaPDF, "_blank");
+}
+
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu() {
+  if (menuVisible) {
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
+}
+
+function seleccionar() {
+  //oculto el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
